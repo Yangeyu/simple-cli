2ae9de441c38d5e58008d2588ae80ed342d5bbe0
yang <yang@yangwb-MacPro.local>
2023-11-17 22:36:22 +0800

    feat: ✨ windicss line clamp

--- windi.config.ts
+++ windi.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'windicss/helpers'
 import plugin from 'windicss/plugin'
 import { DeepNestObject } from 'windicss/types/interfaces'
 import { Utilities } from './src/styles/windi/utilities'
+import lineClamp from 'windicss/plugin/line-clamp'
 
 export default defineConfig({
   attributify: true,
@@ -10,9 +11,14 @@ export default defineConfig({
       colors: {
         primary: 'blue',
       },
+      lineClamp: {
+        sm: '2',
+        lg: '10'
+      }
     },
   },
   plugins: [
+    lineClamp,
     plugin(({ addUtilities }) => {
       addUtilities(Utilities as DeepNestObject)
     }),
